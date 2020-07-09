fafc357eb161cc1c6959e3512b7a3f16e1af2bc5
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-09 14:58:03 +0100

    Remove generated config.js

--- app/public/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-angular.module('config', [])
-
-.constant('ENV', {environment:'development',baseUrl:'http://dev.ps2alerts.com',apiUrl:'http://api.dev.ps2alerts.com/v2',websocketUrl:'ws://wss.dev.ps2alerts.com?apikey=FOOBAR'})
-
-;
