77effdeacc690257237a7d947e89b9b48f692757
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-11 14:45:23 +0100

    Correct terraform format

--- main.tf
+++ main.tf
@@ -12,33 +12,33 @@ data "digitalocean_kubernetes_cluster" "cluster" {
 
 provider "kubernetes" {
   load_config_file = false
-  host = data.digitalocean_kubernetes_cluster.cluster.endpoint
-  token = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
+  host             = data.digitalocean_kubernetes_cluster.cluster.endpoint
+  token            = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
   cluster_ca_certificate = base64decode(
     data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
   )
 }
 
 resource "digitalocean_kubernetes_cluster" "mycluster" {
-  name = "my-cluster"
-  region = "lon1"
+  name    = "my-cluster"
+  region  = "lon1"
   version = "1.17.6-do.0"
 
   node_pool {
-    name = "web-pool"
-    size = "s-1vcpu-2gb"
+    name       = "web-pool"
+    size       = "s-1vcpu-2gb"
     node_count = 2
-    min_nodes = 1
-    max_nodes = 3
+    min_nodes  = 1
+    max_nodes  = 3
   }
 }
 
 module "website_staging" {
-  source = "./provisioning/terraform/modules/website"
-  namespace = "ps2alerts-website"
-  environment = "staging"
-  identifier = "ps2alerts-website-staging"
-  url = "staging.ps2alerts.com"
-  api_url = "api.staging.ps2alerts.com"
+  source           = "./provisioning/terraform/modules/website"
+  namespace        = "ps2alerts-website"
+  environment      = "staging"
+  identifier       = "ps2alerts-website-staging"
+  url              = "staging.ps2alerts.com"
+  api_url          = "api.staging.ps2alerts.com"
   checksum_version = var.checksum_version
 }
