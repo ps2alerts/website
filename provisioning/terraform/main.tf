884231b7fd4333272a709923c18b5638a38e75a1
Matt Cavanagh <maelstrome26@gmail.com>
2020-07-11 18:15:39 +0100

    God damn formatting

--- provisioning/terraform/main.tf
+++ provisioning/terraform/main.tf
@@ -15,7 +15,7 @@ provider "kubernetes" {
   host             = data.digitalocean_kubernetes_cluster.cluster.endpoint
   token            = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
   cluster_ca_certificate = base64decode(
-  data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
+    data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
   )
 }
 
