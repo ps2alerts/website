cf9c0456647399e457289bd5ef347d662f805d50
Matt <maelstrome26@gmail.com>
2020-08-14 01:17:32 +0100

    Bloody TF formatting...

--- provisioning/terraform/staging.tf
+++ provisioning/terraform/staging.tf
@@ -1,14 +1,14 @@
 module "website_staging" {
-  source             = "./modules/website"
-  namespace          = "ps2alerts"
-  environment        = "staging"
-  identifier         = "ps2alerts-website-staging"
-  url                = "staging.ps2alerts.com"
-  checksum_version   = var.checksum_version
-  api_host           = "api.staging.ps2alerts.com"
-  api_token          = var.api_token
-  cpu_limit          = "250m"
-  mem_limit          = "128Mi"
-  cpu_request        = "125m"
-  mem_request        = "64Mi"
+  source           = "./modules/website"
+  namespace        = "ps2alerts"
+  environment      = "staging"
+  identifier       = "ps2alerts-website-staging"
+  url              = "staging.ps2alerts.com"
+  checksum_version = var.checksum_version
+  api_host         = "api.staging.ps2alerts.com"
+  api_token        = var.api_token
+  cpu_limit        = "250m"
+  mem_limit        = "128Mi"
+  cpu_request      = "125m"
+  mem_request      = "64Mi"
 }
