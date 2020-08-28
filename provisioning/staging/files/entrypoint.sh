f6ed8aa3693d10528302625cf0730e22019b7d65
Matt Cavanagh <maelstrome26@gmail.com>
2020-08-28 23:08:38 +0100

    Removed staging entrypoint for now

--- provisioning/staging/files/entrypoint.sh
+++ provisioning/staging/files/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# Not used for now, since it's just NGINX serving static assets!
 echo "=============== STARTING WEBSITE (STAGING) ==================="
-pwd
+sudo --preserve-env=ENVIRONMENT \
+  --preserve-env=VERSION \
+  --preserve-env=BASE_URL \
+  --preserve-env=API_URL \
+  -u root runsvdir -P /etc/service
