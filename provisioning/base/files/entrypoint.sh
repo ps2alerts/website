8ee17ec41436627e345d6f5162006799ca0eaff0
Matt Cavanagh <maelstrome26@gmail.com>
2020-08-28 22:51:54 +0100

    Reverted entrypoint

--- provisioning/base/files/entrypoint.sh
+++ provisioning/base/files/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 echo "=============== STARTING SERVICES ==================="
-runsvdir -P /etc/service
+sudo --preserve-env=ENVIRONMENT \
+  --preserve-env=VERSION \
+  --preserve-env=BASE_URL \
+  --preserve-env=API_URL \
+  -u root runsvdir -P /etc/service
