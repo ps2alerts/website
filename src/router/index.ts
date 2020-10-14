d512e0c7b31c9a57f22c12d6b73cfa7d80847681
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-15 00:28:05 +0100

    Working dev environment via containers and using dev.ps2alerts.com. Implemented 404 mechanism

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    // Serve 404s on unknown routes
+    name: 'not-found',
+    path: '/:pathMatch(.*)*',
+    component: NotFound
   }
 ]
 
