6a18963f69921fc3f64f53c4f6e19fb062281277
Matt Cavanagh <maelstrome26@gmail.com>
2020-10-24 00:28:37 +0100

    Apparently --preserve-env is no longer required to send arguments to

--- provisioning/base/entrypoint.sh
+++ provisioning/base/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 echo "=============== STARTING WEBSITE SERVICES ==================="
-sudo --preserve-env=ENVIRONMENT \
-  --preserve-env=VERSION \
-  --preserve-env=API_URL -u root runsvdir -P /etc/service
+sudo --preserve-env root runsvdir -P /etc/service
