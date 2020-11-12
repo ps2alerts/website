23f432234220fa82b462c39e317125d27b78bccc
Matt Cavanagh <maelstrome26@gmail.com>
2020-11-12 03:15:03 +0000

    Playing around with the homepage elements and flexibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,130 @@
 <template>
-  <div class="col-span-3 ss:col-span-4 h-full flex items-center justify-center bg-gray-300">
-    <div class="text-center">
-      <h1 class="text-3xl">
-        Global Alert Statistics
+  <div class="col-span-3 ss:col-span-4">
+    <div class="text-center h-full">
+      <h1 class="text-title">
+        Alert Statistics
       </h1>
-      <p>This page is heavily WIP!</p>
+      <p>All data is currently subject to wipe!</p>
     </div>
   </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    1
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    2
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    3
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    4
-  </div>
-  <div class="h-full ss:col-start-2 flex items-center justify-center bg-gray-300">
-    5
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    6
-  </div>
-  <div class="col-span-3 ss:col-span-4 h-full flex items-center justify-center bg-gray-300">
+  <div class="col-span-3 ss:col-span-4 card">
     <div class="text-center">
       <h1>
-        Global Combat Statistics
+        Victory Statistics
       </h1>
-      <p>This page is heavily WIP!</p>
+      <p>TO BE FINISHED!</p>
     </div>
   </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    1
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    2
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    3
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    4
-  </div>
-  <div class="h-full ss:col-start-2 flex items-center justify-center bg-gray-300">
-    5
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    6
-  </div>
-  <div class="col-span-3 ss:col-span-4 h-full flex items-center justify-center bg-gray-300">
+  <div class="col-span-3 ss:col-span-4 card">
     <div class="text-center">
-      <h1 class="text-3xl">
-        Server Statistics
+      <h1>
+        Combat Statistics
       </h1>
-      <p>This page is heavily WIP!</p>
+      <p>TO BE FINISHED!</p>
+    </div>
+    <div class="flex justify-center content-center flex-wrap">
+      <div class="flex-1 metric-card-fw bg-tint-light text-center">
+        <h2 class="text-2xl">
+          Kills
+        </h2>
+        <div class="vs">
+          12345
+        </div>
+        <div class="nc">
+          12345
+        </div>
+        <div class="tr">
+          12345
+        </div>
+        <div class="nso">
+          12345
+        </div>
+      </div>
+      <div class="flex-1 metric-card-fw bg-tint-light text-center">
+        <h2 class="text-2xl">
+          Deaths
+        </h2>
+        <div class="vs">
+          12345
+        </div>
+        <div class="nc">
+          12345
+        </div>
+        <div class="tr">
+          12345
+        </div>
+        <div class="nso">
+          12345
+        </div>
+      </div>
+      <div class="flex-1 metric-card-fw bg-tint-light text-center">
+        <h2 class="text-2xl">
+          Teamkills
+        </h2>
+        <div class="vs">
+          12345
+        </div>
+        <div class="nc">
+          12345
+        </div>
+        <div class="tr">
+          12345
+        </div>
+        <div class="nso">
+          12345
+        </div>
+      </div>
+      <div class="flex-1 metric-card-fw bg-tint-light text-center">
+        <h2 class="text-2xl">
+          Suicides
+        </h2>
+        <div class="vs">
+          12345
+        </div>
+        <div class="nc">
+          12345
+        </div>
+        <div class="tr">
+          12345
+        </div>
+        <div class="nso">
+          12345
+        </div>
+      </div>
+      <div class="flex-1 metric-card-fw bg-tint-light text-center">
+        <h2 class="text-2xl">
+          Headshots
+        </h2>
+        <div class="vs">
+          12345
+        </div>
+        <div class="nc">
+          12345
+        </div>
+        <div class="tr">
+          12345
+        </div>
+        <div class="nso">
+          12345
+        </div>
+      </div>
+    </div>
+    <hr class="my-4 mx-2">
+    <div class="flex justify-center content-center">
+      <div class="flex-1 text-center">
+        <h2 class="text-2xl">
+          Server metrics
+        </h2>
+      </div>
     </div>
   </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    1
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    2
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    3
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    4
-  </div>
-  <div class="h-full ss:col-start-2 flex items-center justify-center bg-gray-300">
-    5
-  </div>
-  <div class="h-full flex items-center justify-center bg-gray-300">
-    6
+  <div class="col-span-3 ss:col-span-4 card">
+    <div class="text-center">
+      <h1>
+        Combat Statistics
+      </h1>
+      <p>TO BE FINISHED!</p>
+    </div>
   </div>
 </template>
 
