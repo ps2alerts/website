7f11ef432ebcaf6040c11d2b70df60899b6c6d48
Matt Cavanagh <maelstrome26@gmail.com>
2020-11-21 16:03:10 +0000

    Updated changelog for alert history

--- src/views/Changelog.vue
+++ src/views/Changelog.vue
@@ -13,6 +13,37 @@
     </div>
     <div class="h-full">
       <h1 class="text-subtitle text-center">
+        Filterable Alert History
+      </h1>
+      <p class="content-spacing text-center">
+        Added: 21st Nov 2020
+      </p>
+      <img
+        class="object-contain mx-auto content-spacing rounded-lg"
+        src="../assets/img/change-log/alert-history.png"
+        alt="Picture showing alert history example"
+      >
+      <p>
+        Filterable Alert History is now available! This shows a list of alerts filtered down by a variety of options. This list continuously updates over time as alerts come in based off your criteria. You're able to select the below criteria:
+      </p>
+      <ul>
+        <li>Server</li>
+        <li>Continent</li>
+        <li>Time Bracket (Mornings = 00:00-11:59, Afternoons = 12:00-15:59, Prime = 16:00-00:00)</li>
+        <li>Victor</li>
+        <li>Date &amp; Time Range</li>
+      </ul>
+      <p>
+        Note this is currently not updated in real time (every 30 secs), and there's a known issue when the data is refreshed where it bounces you back to the top, that will be fixed in due course.
+      </p>
+    </div>
+  </div>
+  <div class="col-span-3 ss:col-span-2 bg-tint card editorial relative">
+    <div class="tag feature">
+      New feature
+    </div>
+    <div class="h-full">
+      <h1 class="text-subtitle text-center">
         Real Time Monitor
       </h1>
       <p class="content-spacing text-center">
@@ -21,8 +52,9 @@
       <img
         class="object-contain mx-auto content-spacing rounded-lg"
         src="../assets/img/change-log/rtm.png"
-        height="213"
-        width="479"
+        alt="Real Time Monitor example"
+        height="322"
+        width="441"
       >
       <p>
         The Real Time Alert Monitor has returned! At the left of your screen (top if you're on mobile) you're now able to see a live list of alerts!
@@ -33,62 +65,62 @@
     </div>
   </div>
 
-  <div class="col-span-3 ss:col-span-2 bg-tint card editorial relative">
-    <div class="tag enhancement">
-      Enhancement
-    </div>
-    <div class="h-full">
-      <h1 class="text-subtitle text-center">
-        Example Enhancement
-      </h1>
-      <p class="content-spacing text-center">
-        Added: 8th Nov 2020
-      </p>
-      <p>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae elit quam. Maecenas enim ipsum, ornare elementum pulvinar et, vestibulum eu eros. Nullam bibendum vulputate mi. Nulla sed felis at enim euismod ornare. Morbi semper felis eget nunc convallis, ac aliquet quam porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas vitae tortor lorem. Phasellus non neque neque. Etiam placerat enim ipsum, porttitor gravida dui imperdiet malesuada. Pellentesque sit amet nulla mollis, tristique dolor id, posuere neque. Praesent lectus elit, consectetur ac nibh id, sollicitudin pulvinar leo. Vivamus venenatis risus neque, sed euismod sem finibus ut. Quisque laoreet ipsum neque, id aliquam ex feugiat nec. Mauris dapibus, nibh nec faucibus sodales, lacus mi pellentesque nulla, quis elementum nunc augue sit amet nisl. Phasellus nec felis et erat vulputate molestie.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet nulla dui, sed finibus purus sodales sit amet. In vitae risus velit. Phasellus sit amet scelerisque ligula. Nullam eget sapien ut lectus lobortis facilisis sit amet nec odio. Suspendisse convallis justo id neque iaculis molestie. Nunc tincidunt nisl sit amet ultricies facilisis. Vestibulum purus turpis, accumsan eu accumsan eu, congue id mauris. Nulla vulputate metus in neque placerat eleifend. Integer pellentesque luctus hendrerit.
-      </p>
-    </div>
-  </div>
+<!--  <div class="col-span-3 ss:col-span-2 bg-tint card editorial relative">-->
+<!--    <div class="tag enhancement">-->
+<!--      Enhancement-->
+<!--    </div>-->
+<!--    <div class="h-full">-->
+<!--      <h1 class="text-subtitle text-center">-->
+<!--        Example Enhancement-->
+<!--      </h1>-->
+<!--      <p class="content-spacing text-center">-->
+<!--        Added: 8th Nov 2020-->
+<!--      </p>-->
+<!--      <p>-->
+<!--        Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus vitae elit quam. Maecenas enim ipsum, ornare elementum pulvinar et, vestibulum eu eros. Nullam bibendum vulputate mi. Nulla sed felis at enim euismod ornare. Morbi semper felis eget nunc convallis, ac aliquet quam porttitor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas vitae tortor lorem. Phasellus non neque neque. Etiam placerat enim ipsum, porttitor gravida dui imperdiet malesuada. Pellentesque sit amet nulla mollis, tristique dolor id, posuere neque. Praesent lectus elit, consectetur ac nibh id, sollicitudin pulvinar leo. Vivamus venenatis risus neque, sed euismod sem finibus ut. Quisque laoreet ipsum neque, id aliquam ex feugiat nec. Mauris dapibus, nibh nec faucibus sodales, lacus mi pellentesque nulla, quis elementum nunc augue sit amet nisl. Phasellus nec felis et erat vulputate molestie.-->
+<!--      </p>-->
+<!--      <p>-->
+<!--        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet nulla dui, sed finibus purus sodales sit amet. In vitae risus velit. Phasellus sit amet scelerisque ligula. Nullam eget sapien ut lectus lobortis facilisis sit amet nec odio. Suspendisse convallis justo id neque iaculis molestie. Nunc tincidunt nisl sit amet ultricies facilisis. Vestibulum purus turpis, accumsan eu accumsan eu, congue id mauris. Nulla vulputate metus in neque placerat eleifend. Integer pellentesque luctus hendrerit.-->
+<!--      </p>-->
+<!--    </div>-->
+<!--  </div>-->
 
-  <div class="col-span-3 ss:col-span-2 bg-tint card editorial relative">
-    <div class="tag fix">
-      Bugfix
-    </div>
-    <div class="h-full">
-      <h1 class="text-subtitle text-center">
-        Example Bugfix
-      </h1>
-      <p class="content-spacing text-center">
-        Added: 8th Nov 2020
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet nulla dui, sed finibus purus sodales sit amet. In vitae risus velit. Phasellus sit amet scelerisque ligula. Nullam eget sapien ut lectus lobortis facilisis sit amet nec odio. Suspendisse convallis justo id neque iaculis molestie. Nunc tincidunt nisl sit amet ultricies facilisis. Vestibulum purus turpis, accumsan eu accumsan eu, congue id mauris. Nulla vulputate metus in neque placerat eleifend. Integer pellentesque luctus hendrerit.
-      </p>
-      <p>
-        Aenean feugiat consectetur nulla eu bibendum. Duis rutrum arcu sed congue laoreet. Phasellus et lectus ac orci eleifend gravida. Morbi congue justo id mollis ultricies. Vestibulum at eleifend enim, eget viverra velit. Donec nec tellus congue, suscipit dui eget, varius sem. Vivamus suscipit magna risus, at condimentum velit pretium vitae. Donec sed orci mi. Morbi accumsan massa vel eros lacinia faucibus.
-      </p>
-    </div>
-  </div>
+<!--  <div class="col-span-3 ss:col-span-2 bg-tint card editorial relative">-->
+<!--    <div class="tag fix">-->
+<!--      Bugfix-->
+<!--    </div>-->
+<!--    <div class="h-full">-->
+<!--      <h1 class="text-subtitle text-center">-->
+<!--        Example Bugfix-->
+<!--      </h1>-->
+<!--      <p class="content-spacing text-center">-->
+<!--        Added: 8th Nov 2020-->
+<!--      </p>-->
+<!--      <p>-->
+<!--        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam imperdiet nulla dui, sed finibus purus sodales sit amet. In vitae risus velit. Phasellus sit amet scelerisque ligula. Nullam eget sapien ut lectus lobortis facilisis sit amet nec odio. Suspendisse convallis justo id neque iaculis molestie. Nunc tincidunt nisl sit amet ultricies facilisis. Vestibulum purus turpis, accumsan eu accumsan eu, congue id mauris. Nulla vulputate metus in neque placerat eleifend. Integer pellentesque luctus hendrerit.-->
+<!--      </p>-->
+<!--      <p>-->
+<!--        Aenean feugiat consectetur nulla eu bibendum. Duis rutrum arcu sed congue laoreet. Phasellus et lectus ac orci eleifend gravida. Morbi congue justo id mollis ultricies. Vestibulum at eleifend enim, eget viverra velit. Donec nec tellus congue, suscipit dui eget, varius sem. Vivamus suscipit magna risus, at condimentum velit pretium vitae. Donec sed orci mi. Morbi accumsan massa vel eros lacinia faucibus.-->
+<!--      </p>-->
+<!--    </div>-->
+<!--  </div>-->
 
-  <div class="col-span-3 ss:col-span-2 bg-tint card editorial relative">
-    <div class="tag known-issue">
-      Known issue
-    </div>
-    <div class="h-full">
-      <h1 class="text-subtitle text-center">
-        Example Known Issue
-      </h1>
-      <p class="content-spacing text-center">
-        Added: 8th Nov 2020
-      </p>
-      <p>
-        Aenean feugiat consectetur nulla eu bibendum. Duis rutrum arcu sed congue laoreet. Phasellus et lectus ac orci eleifend gravida. Morbi congue justo id mollis ultricies. Vestibulum at eleifend enim, eget viverra velit. Donec nec tellus congue, suscipit dui eget, varius sem. Vivamus suscipit magna risus, at condimentum velit pretium vitae. Donec sed orci mi. Morbi accumsan massa vel eros lacinia faucibus.
-      </p>
-    </div>
-  </div>
+<!--  <div class="col-span-3 ss:col-span-2 bg-tint card editorial relative">-->
+<!--    <div class="tag known-issue">-->
+<!--      Known issue-->
+<!--    </div>-->
+<!--    <div class="h-full">-->
+<!--      <h1 class="text-subtitle text-center">-->
+<!--        Example Known Issue-->
+<!--      </h1>-->
+<!--      <p class="content-spacing text-center">-->
+<!--        Added: 8th Nov 2020-->
+<!--      </p>-->
+<!--      <p>-->
+<!--        Aenean feugiat consectetur nulla eu bibendum. Duis rutrum arcu sed congue laoreet. Phasellus et lectus ac orci eleifend gravida. Morbi congue justo id mollis ultricies. Vestibulum at eleifend enim, eget viverra velit. Donec nec tellus congue, suscipit dui eget, varius sem. Vivamus suscipit magna risus, at condimentum velit pretium vitae. Donec sed orci mi. Morbi accumsan massa vel eros lacinia faucibus.-->
+<!--      </p>-->
+<!--    </div>-->
+<!--  </div>-->
 </template>
 
 <script lang="ts">
@@ -103,6 +135,11 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+ul {
+  list-style-type: disc;
+  margin-left: 1rem;
+  margin-bottom: 1rem;
+}
 %tag {
   @apply py-1 px-2 rounded-tl rounded-tr text-xs text-center font-semibold;
   margin: -1rem;
@@ -128,5 +165,4 @@ export default defineComponent({
   @extend %tag;
   @apply bg-yellow-800;
 }
-
 </style>
