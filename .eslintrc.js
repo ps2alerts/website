6a45f0d788723328d711de8a17c89c5ba041e463
Matt Cavanagh <maelstrome26@gmail.com>
2020-12-05 19:47:39 +0000

    Rejigged build process on staging. Added additional linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,11 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/no-useless-v-bind': 'error',
+    'vue/require-name-property': 'error',
+  },
   plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
 }
