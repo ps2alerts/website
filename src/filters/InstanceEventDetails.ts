46343fc0eb34f31c9096c0f3b497f94defaa4010
Matt Cavanagh <maelstrome26@gmail.com>
2020-12-05 19:48:23 +0000

    Deleted leftover files

--- src/filters/InstanceEventDetails.ts
+++
@@ -1,8 +0,0 @@
-import {metagameEventTypeDetailsMap} from "@/constants/MetagameEventType";
-import {MetagameDetailsInterface} from "@/interfaces/MetagameDetailsInterface";
-
-export const InstanceEventDetails = (eventTypeId: number): MetagameDetailsInterface | null => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-  // @ts-ignore
-  return metagameEventTypeDetailsMap.has(eventTypeId) ? metagameEventTypeDetailsMap.get(eventTypeId) : null
-}
