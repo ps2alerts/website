76b16bb8cd140bf0e5c4decd0d97954726340732
Matt Cavanagh <maelstrome26@gmail.com>
2021-01-06 02:53:53 +0000

    Fixed interface for AlertCharacterTableDataInterface

--- interfaces/alert/AlertCharacterTableDataInterface.ts
+++ interfaces/alert/AlertCharacterTableDataInterface.ts
@@ -4,5 +4,5 @@ export interface AlertCharacterTableDataInterface
   extends InstanceCharacterAggregateResponseInterface {
   kd: string | number
   hsr: string | number
-  br: string
+  br: number
 }
