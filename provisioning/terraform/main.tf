1357d36699687f1611139997829af276d35b9ce9
Matt Cavanagh <maelstrome26@gmail.com>
2021-01-25 19:07:16 +0000

    Terraform formatting

--- provisioning/terraform/main.tf
+++ provisioning/terraform/main.tf
@@ -11,9 +11,9 @@ data "digitalocean_kubernetes_cluster" "cluster" {
 }
 
 provider "kubernetes" {
-  host             = data.digitalocean_kubernetes_cluster.cluster.endpoint
-  token            = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
+  host  = data.digitalocean_kubernetes_cluster.cluster.endpoint
+  token = data.digitalocean_kubernetes_cluster.cluster.kube_config[0].token
   cluster_ca_certificate = base64decode(
-    data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
+  data.digitalocean_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate
   )
 }
