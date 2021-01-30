ad4d0fe12414459c51feb3fe410358d601c1e75b
Matt Cavanagh <maelstrome26@gmail.com>
2021-01-30 19:15:39 +0000

    Introduced realtime stats for streaming overlay

--- api-request.ts
+++ api-request.ts
@@ -20,7 +20,6 @@ export default class ApiRequest {
         params,
       })
       .then((response) => {
-        console.log('Api Client Request', { endpoint, params, response })
         return response.data
       })
   }
