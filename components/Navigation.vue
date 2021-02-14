8f46abe42fe810ce0287c063ae18c0c04db3ec10
Matt Cavanagh <maelstrome26@gmail.com>
2021-02-14 16:37:48 +0000

    Shuffled navigation buttons round in level of importance

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -14,18 +14,12 @@
 
     <div>
       <a
-        href="https://github.com/ps2alerts"
+        href="https://www.patreon.com/PS2Alerts"
         class="btn btn-sm btn-alt mb-2"
         target="_blank"
+        style="box-shadow: 0 0 5px #ccc"
       >
-        <font-awesome-icon :icon="['fab', 'github']" /> GitHub
-      </a>
-      <a
-        href="https://twitter.com/ps2alerttracker"
-        class="btn btn-sm btn-alt mb-2"
-        target="_blank"
-      >
-        <font-awesome-icon :icon="['fab', 'twitter']" /> Twitter
+        <font-awesome-icon :icon="['fab', 'patreon']" /> Support the project!
       </a>
       <a
         href="https://discord.io/ps2alerts"
@@ -35,12 +29,18 @@
         <font-awesome-icon :icon="['fab', 'discord']" /> Discord
       </a>
       <a
-        href="https://www.patreon.com/PS2Alerts"
+        href="https://twitter.com/ps2alerttracker"
         class="btn btn-sm btn-alt mb-2"
         target="_blank"
-        style="box-shadow: 0 0 5px #ccc"
       >
-        <font-awesome-icon :icon="['fab', 'patreon']" /> Support the project!
+        <font-awesome-icon :icon="['fab', 'twitter']" /> Twitter
+      </a>
+      <a
+        href="https://github.com/ps2alerts"
+        class="btn btn-sm btn-alt mb-2"
+        target="_blank"
+      >
+        <font-awesome-icon :icon="['fab', 'github']" /> GitHub
       </a>
     </div>
   </nav>
