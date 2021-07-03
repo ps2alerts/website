40ce2c7c2ab0a7e6f25be4c8ffa9176ed28c79a7
Matt Cavanagh <maelstrome26@gmail.com>
2021-07-03 23:51:01 +0100

    Formatted announcement better

--- components/Announcement.vue
+++ components/Announcement.vue
@@ -7,15 +7,15 @@
       Notice - 2nd July 2021
     </div>
     <div>
-      <h1>Prime Alerts are currently not recording correctly</h1>
+      <h1 class="text-3xl mb-4 text-center">Prime Alerts are currently not recording correctly</h1>
       <p>
-        Due to a Census (PS2 API) issue, we are unable to accurately determine the faction NSO players are playing for. Since the launch of the NSO update, this has majorly borked the population acticity levels as we cannot identify factions for players. Pre-NSO update this wasn't an issue as there was maybe 20 players at a prime time alert, now there's around 300.
+        Due to an Census (PS2 API) issue, we are unable to accurately determine the faction NSO players are playing for. Since the launch of the NSO update, this has majorly borked the population activity levels as we cannot identify factions for players. Pre-NSO update this wasn't an issue as there was maybe 20 players at a prime time alert, now there's around 300.
       </p>
       <p>
         This will not be fixed until Census has been changed. I'm also not willing to "bodge" a fix using NSO pops by attempting to calculate who they are playing on as it is inherently inaccurate. When the NSO hype dies down, this issue will naturally dissipate.
       </p>
       <p>
-        - Maelstrome26, PS2Alerts Developer
+        - <b>Maelstrome26</b> - PS2Alerts Developer
       </p>
     </div>
   </div>
