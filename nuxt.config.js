3a3b7e6d852a066cca83e664e54c8b64821645c7
Matt Cavanagh <maelstrome26@gmail.com>
2022-01-19 00:10:16 +0000

    Fixed components not loading

--- nuxt.config.js
+++ nuxt.config.js
@@ -108,7 +108,7 @@ export default {
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+  components: [{ path: '~/components', pathPrefix: false }],
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
