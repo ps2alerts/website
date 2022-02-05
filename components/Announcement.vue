3132e70db050fe281b5a658179c66949848015ef
Matt Cavanagh <maelstrome26@gmail.com>
2022-02-05 17:50:17 +0000

    Updated announcement

--- components/Announcement.vue
+++ components/Announcement.vue
@@ -4,35 +4,16 @@
   >
     <div class="tag announcement">
       <font-awesome-icon :icon="['fas', 'bullhorn']"></font-awesome-icon>
-      Notice - 2nd Feb 2022
+      Notice - 6th Feb 2022
     </div>
-    <div class="text-left">
-      <h1 class="mb-4 text-center">Oshur support nearly finished!</h1>
+    <div class="text-center">
+      <h1>Oshur is now supported!</h1>
       <p>
-        Oshur support is still being developed! The collection system for
-        PS2Alerts now has a working prototype with the new Oshur alerts, which
-        is big step towards full Oshur support!
+        Oshur support is now live! Development of Oshur support was delayed due
+        to lack of support from the game's API team, we had to do a lot of
+        reverse engineering in order to make this work. Many thanks to the
+        player API community on the PS2 Discord in the #api-dev channel.
       </p>
-      <p>
-        Unfortunately, due to the lack of support from the game's API (it's
-        missing data since Containment Sites were added) it is hampering
-        development efforts and us API devs are having to do a lot of
-        <a
-          href="https://github.com/ps2alerts/aggregator/blob/revive/src/constants/lattice/344-reverse-engineered.json"
-          class="text-red-500 hover:underline"
-          >data scraping </a
-        >from the game's files.
-        <a
-          href="https://github.com/ps2alerts/aggregator/commit/05acc299f9308ffde8395559b5e65bbfdd642bc1#diff-796089b7fb5c6121c15141b3e0a176527874b01ad8bb751af06c93833de0c70eR89-R107"
-          class="text-red-500 hover:underline"
-          >Click to witness pain in code form.</a
-        >
-      </p>
-      <p>
-        I'm hoping to have full Oshur support online by the end of the coming
-        weekend!
-      </p>
-      <p>- Maelstrome26 (PS2Alerts Developer)</p>
     </div>
   </div>
 </template>
