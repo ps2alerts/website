bb24cc7539041e324ded1b73b9ae653f672f64f5
Ryan Sims <ryanjsims@email.arizona.edu>
2022-03-10 21:43:52 -0700

    Weird tangent I took while making cutoff territory
    Initially I wanted to make cutoff territory have stripes run through it
    As soon as I finished implementing and fixing enough bugs
    to get it to work, I remembered that in game the color just changes...
    So here's the work that I spent a fair amount of time on lol

--- libraries/MapRegion.ts
+++ libraries/MapRegion.ts
@@ -98,6 +98,110 @@ export class MapRegion implements MapRegionDrawingInterface {
         return this.indices;
     }
 
+    private intersection(edge: number[][], slope: number, start: number[]): number[]{
+        var maxX = Math.max(edge[1][0], edge[0][0]);
+        var minX = Math.min(edge[1][0], edge[0][0]);
+        var maxY = Math.max(edge[1][1], edge[0][1]);
+        var minY = Math.min(edge[1][1], edge[0][1]);
+        
+        if(Math.abs(maxX - minX) < 0.1){
+            var y = slope * (edge[1][0] - start[0]) + start[1];
+            if(y < maxY && y > minY){
+                return [minX, y];
+            }
+        } else {
+            var edgeStart = (minX == edge[1][0]) ? [minX, edge[1][1]] : [minX, edge[0][1]];
+            var edgeEnd = (maxX == edge[1][0]) ? [maxX, edge[1][1]] : [maxX, edge[0][1]];
+            var slopeEdge = (edgeEnd[1] - edgeStart[1]) / (edgeEnd[0] - edgeStart[0]);
+            if(Math.abs(slope - slopeEdge) < 0.01)
+                return [];
+            var xint = (start[1] - edgeStart[1] - slope * start[0] + slopeEdge * edgeStart[0]) / (slopeEdge - slope);
+            var yint = slope * (xint - start[0]) + start[1];
+            if((Math.abs(maxY - minY) < 0.1 && Math.abs(maxY - yint) < 0.1 && minX < xint && xint < maxX)
+                || (minY < yint && yint < maxY))
+                return [xint, yint];
+        }
+        return [];
+    }
+
+    intersections(start: number[], angle: number): number[][] {
+        if(Math.abs(angle) == Math.PI / 2){
+            return [];
+        }
+        var intersects: number[][] = []
+        var slope = Math.sin(angle) / Math.cos(angle);
+        var intersection = this.intersection([this.outline_cache[0], this.outline_cache[this.outline_cache.length - 1]], slope, start);
+        for(var i = 1; i < this.outline().length; i++){
+            if(intersection.length !== 0){
+                intersects.push(intersection);
+            }
+            intersection = this.intersection([this.outline_cache[i], this.outline_cache[i - 1]], slope, start);
+        }
+        if(intersection.length !== 0){
+            intersects.push(intersection);
+        }
+        return intersects;
+    }
+
+    stripes(angle: number): LatLng[][] {
+        angle = Math.min(Math.PI, Math.max(0, angle));
+        var min = [this.outline()[0][0], this.outline()[0][1]];
+        var max = [this.outline()[0][0], this.outline()[0][1]];
+        this.outline().forEach((point) => {
+            min[0] = Math.min(point[0], min[0]);
+            min[1] = Math.min(point[1], min[1]);
+            max[0] = Math.max(point[0], max[0]);
+            max[1] = Math.max(point[1], max[1]);
+        });
+        var toReturn: LatLng[][] = [];
+        var start = [Math.floor(min[0] / 25) * 25, min[1] - 1];
+        var point = [Math.floor(min[0] / 25) * 25, min[1] - 1];
+        while(point[0] < max[0]){
+            var intersections = this.intersections(point, angle);
+            intersections.sort((a, b) => {
+                if(a[0] < b[0]) {
+                    return -1;
+                }
+                if(Math.abs(a[0] - b[0]) < 0.01) {
+                    return 0;
+                }
+                return 1;
+            });
+            for(var i = 0; i < intersections.length - 1; i++){
+                if(!(intersections[i][0] < intersections[i+1][0])){
+                    var temp = intersections[i];
+                    intersections[i] = intersections[i+1];
+                    intersections[i+1] = temp;
+                }
+                var inRegion = [
+                    this.contains([intersections[i][0], intersections[i][1]]),
+                    this.contains([intersections[i][0]+1, intersections[i][1]+1])
+                ].some((value) => value);
+                if(inRegion){
+                    toReturn.push([worldToMap(intersections[i]), worldToMap(intersections[i + 1])]);
+                    i++;
+                }
+            }
+            point[0] = point[0] + 25;
+        }
+        point[0] = start[0];
+        while(point[1] < max[1]){
+            var intersections = this.intersections(point, angle);
+            for(var i = 0; i < intersections.length - 1; i++){
+                var inRegion = [
+                    this.contains([intersections[i][0], intersections[i][1]]),
+                    this.contains([intersections[i][0]+1, intersections[i][1]+1])
+                ].some((value) => value);
+                if(inRegion){
+                    toReturn.push([worldToMap(intersections[i]), worldToMap(intersections[i + 1])]);
+                    i++;
+                }
+            }
+            point[1] = point[1] + 25;
+        }
+        return toReturn;
+    }
+
     contains(point: number[]): boolean {
         var toFind = CubeHex.fromWorld(point[0], point[1], ZoneHexSize(this.zoneId));
         return this.hexSet[toFind.toString()] !== undefined;
