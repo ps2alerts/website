bdbadadb24af1e38de6c40d4350eb1dd545870b8
Matt Cavanagh <maelstrome26@gmail.com>
2022-05-11 20:59:16 +0100

    Removed the notice, added a NoticeBanner component

---
+++ components/NoticeBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="bg-orange-600 w-full p-4 my-4 text-center rounded">
+      <font-awesome-icon
+        :icon="['fas', 'exclamation-triangle']"
+      ></font-awesome-icon>
+      PC Alert tracking is offline. Visit the
+      <a class="text-gray-400 font-bold" href="/">homepage</a> for more details
+      <font-awesome-icon
+        :icon="['fas', 'exclamation-triangle']"
+      ></font-awesome-icon>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NoticeBanner',
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -30,19 +30,7 @@
               ></font-awesome-icon>
             </div>
           </div>
-          <div>
-            <div class="bg-orange-600 w-full p-4 my-4 text-center rounded">
-              <font-awesome-icon
-                :icon="['fas', 'exclamation-triangle']"
-              ></font-awesome-icon>
-              PC Alert tracking is offline. Visit the
-              <a class="text-gray-400 font-bold" href="/">homepage</a> for more
-              details
-              <font-awesome-icon
-                :icon="['fas', 'exclamation-triangle']"
-              ></font-awesome-icon>
-            </div>
-          </div>
+          <!--<NoticeBanner></NoticeBanner>-->
           <Navigation class="hidden lg:block" />
         </client-only>
         <client-only>
