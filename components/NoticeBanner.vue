d7d46023cdf206b2fa31b7409c79116d469736b3
Matt Cavanagh <maelstrome26@gmail.com>
2022-05-15 01:38:57 +0100

    Updated website bannrer

--- components/NoticeBanner.vue
+++ components/NoticeBanner.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <div class="bg-orange-600 w-full p-4 my-4 text-center rounded">
-      <font-awesome-icon
-        :icon="['fas', 'exclamation-triangle']"
-      ></font-awesome-icon>
-      May 14th 17:50 BST: Database migration is still ongoing, approx 45 mins
-      remaining. During this time the site will not function.
-      <font-awesome-icon
-        :icon="['fas', 'exclamation-triangle']"
-      ></font-awesome-icon>
+    <!--    <div class="bg-orange-600 w-full p-4 my-4 text-center rounded">-->
+    <!--      <font-awesome-icon-->
+    <!--        :icon="['fas', 'exclamation-triangle']"-->
+    <!--      ></font-awesome-icon>-->
+    <!--      May 14th 17:50 BST: Database migration is still ongoing, approx 45 mins-->
+    <!--      remaining. During this time the site will not function.-->
+    <!--      <font-awesome-icon-->
+    <!--        :icon="['fas', 'exclamation-triangle']"-->
+    <!--      ></font-awesome-icon>-->
+    <!--    </div>-->
+    <div class="bg-blue-500 w-full p-4 my-4 text-center rounded">
+      <font-awesome-icon :icon="['fas', 'info-circle']"></font-awesome-icon>
+      On May 14th, there was only a partial recording due to downtime moving
+      databases
+      <font-awesome-icon :icon="['fas', 'info-circle']"></font-awesome-icon>
     </div>
   </div>
 </template>
