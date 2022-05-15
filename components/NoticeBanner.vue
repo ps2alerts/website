cae3182287650f52dc2b738ab5680573886e104a
Matt Cavanagh <maelstrome26@gmail.com>
2022-05-15 11:23:57 +0100

    Added notice RE census

--- components/NoticeBanner.vue
+++ components/NoticeBanner.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <!--    <div class="bg-orange-600 w-full p-4 my-4 text-center rounded">-->
-    <!--      <font-awesome-icon-->
-    <!--        :icon="['fas', 'exclamation-triangle']"-->
-    <!--      ></font-awesome-icon>-->
-    <!--      May 14th 17:50 BST: Database migration is still ongoing, approx 45 mins-->
-    <!--      remaining. During this time the site will not function.-->
-    <!--      <font-awesome-icon-->
-    <!--        :icon="['fas', 'exclamation-triangle']"-->
-    <!--      ></font-awesome-icon>-->
-    <!--    </div>-->
+    <div class="bg-orange-600 w-full p-4 my-4 text-center rounded">
+      <font-awesome-icon
+        :icon="['fas', 'exclamation-triangle']"
+      ></font-awesome-icon>
+      Census (the game's API) continues to remain unstable. We are currently not
+      receiving PC Alert start notifications and thus are not being recorded.
+      <font-awesome-icon
+        :icon="['fas', 'exclamation-triangle']"
+      ></font-awesome-icon>
+    </div>
     <div class="bg-blue-500 w-full p-4 my-4 text-center rounded">
       <font-awesome-icon :icon="['fas', 'info-circle']"></font-awesome-icon>
       On May 14th, there was only a partial recording due to downtime moving
