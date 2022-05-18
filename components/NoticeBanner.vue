74c2bb70b2fe48bb83a342f28e4448168a4ff705
Matt Cavanagh <maelstrome26@gmail.com>
2022-05-18 23:16:05 +0100

    Updated banner again

--- components/NoticeBanner.vue
+++ components/NoticeBanner.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <div class="bg-orange-600 w-full p-4 my-4 text-center rounded">
-      <font-awesome-icon
-        :icon="['fas', 'exclamation-triangle']"
-      ></font-awesome-icon>
-      Alerts are recording again however kill data is unstable. Alert
-      populations, brackets, kills (and other combat data) is currently
-      <b>highly</b> inaccurate, but it appears territory is fine.
-      <font-awesome-icon
-        :icon="['fas', 'exclamation-triangle']"
-      ></font-awesome-icon>
+      <p>
+        <font-awesome-icon
+          :icon="['fas', 'exclamation-triangle']"
+        ></font-awesome-icon>
+        <b>Census (PS2 API) instability continues</b>
+        <font-awesome-icon
+          :icon="['fas', 'exclamation-triangle']"
+        ></font-awesome-icon>
+      </p>
+      <p>
+        Alerts are recording again however kill data is unstable. Alert
+        populations, brackets, kills (and other combat data) is currently
+        <b>highly</b> inaccurate, but it appears territory is fine.
+      </p>
     </div>
     <!--    <div class="bg-blue-500 w-full p-4 my-4 text-center rounded">
       <font-awesome-icon :icon="['fas', 'info-circle']"></font-awesome-icon>
