3bab5e6b85ba697280edd778c2642f373806cce0
Matt Cavanagh <maelstrome26@gmail.com>
2022-05-18 22:38:10 +0100

    Updated banner

--- components/NoticeBanner.vue
+++ components/NoticeBanner.vue
@@ -4,18 +4,19 @@
       <font-awesome-icon
         :icon="['fas', 'exclamation-triangle']"
       ></font-awesome-icon>
-      Census (the game's API) continues to remain unstable. We are currently not
-      receiving PC Alert start notifications and thus are not being recorded.
+      Alerts are recording again however kill data is unstable. Alert
+      populations, brackets, kills (and other combat data) is currently
+      <b>highly</b> inaccurate, but it appears territory is fine.
       <font-awesome-icon
         :icon="['fas', 'exclamation-triangle']"
       ></font-awesome-icon>
     </div>
-    <div class="bg-blue-500 w-full p-4 my-4 text-center rounded">
+    <!--    <div class="bg-blue-500 w-full p-4 my-4 text-center rounded">
       <font-awesome-icon :icon="['fas', 'info-circle']"></font-awesome-icon>
       On May 14th, there was only a partial recording due to downtime moving
       databases, some alerts were not recorded on this date.
       <font-awesome-icon :icon="['fas', 'info-circle']"></font-awesome-icon>
-    </div>
+    </div>-->
   </div>
 </template>
 
