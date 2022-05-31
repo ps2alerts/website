2bf663abfe7556d47f601c77ad8fc2de8c40b57c
Matt Cavanagh <maelstrome26@gmail.com>
2022-05-31 16:20:19 +0100

    Added basic validation to the date pickers so users can't select dates the wrong way round, confusing the API

--- components/common/FilterDate.vue
+++ components/common/FilterDate.vue
@@ -1,117 +1,125 @@
 <template>
-  <div class="flex justify-center items-center">
-    <div id="date-pickers">
-      <div class="mr-1 inline-block">
-        <v-dialog
-          ref="dialogFrom"
-          v-model="modalFrom"
-          dark
-          width="290px"
-          :disabled="disabled"
-        >
-          <template #activator="{ on, attrs }">
-            <v-text-field
-              id="dateFrom"
+  <div>
+    <div class="flex justify-center items-center">
+      <div id="date-pickers">
+        <div class="mr-1 inline-block">
+          <v-dialog
+            ref="dialogFrom"
+            v-model="modalFrom"
+            dark
+            width="290px"
+            :disabled="disabled"
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                id="dateFrom"
+                v-model="dateFrom"
+                :value="dateFrom"
+                :disabled="disabled"
+                prepend-icon="mdi-calendar"
+                background-color="#37474fcc"
+                readonly
+                dark
+                dense
+                filled
+                outlined
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
               v-model="dateFrom"
-              :value="dateFrom"
-              :disabled="disabled"
-              prepend-icon="mdi-calendar"
-              background-color="#37474fcc"
-              readonly
+              scrollable
               dark
-              dense
-              filled
-              outlined
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="dateFrom"
-            scrollable
-            dark
-            color="#A03636"
-            :disabled="disabled"
-            :min="dateStartedRecording"
-            :max="dateNow"
+              color="#A03636"
+              :disabled="disabled"
+              :min="dateStartedRecording"
+              :max="dateNow"
+            >
+              <v-spacer></v-spacer>
+              <v-btn text class="btn" @click="setDateFromToStart()">
+                Start of data
+              </v-btn>
+            </v-date-picker>
+          </v-dialog>
+          <label
+            class="text-center text-sm"
+            for="dateFrom"
+            :class="{ 'text-gray-600': disabled }"
+            >Date from</label
           >
-            <v-spacer></v-spacer>
-            <v-btn text class="btn" @click="setDateFromToStart()">
-              Start of data
-            </v-btn>
-          </v-date-picker>
-        </v-dialog>
-        <label
-          class="text-center text-sm"
-          for="dateFrom"
-          :class="{ 'text-gray-600': disabled }"
-          >Date from</label
-        >
-      </div>
-      <div class="mr-2 inline-block">
-        <v-dialog
-          ref="dialogTo"
-          v-model="modalTo"
-          dark
-          width="290px"
-          :value="dateTo"
-          :disabled="disabled"
-        >
-          <template #activator="{ on, attrs }">
-            <v-text-field
-              id="dateTo"
+        </div>
+        <div class="mr-2 inline-block">
+          <v-dialog
+            ref="dialogTo"
+            v-model="modalTo"
+            dark
+            width="290px"
+            :value="dateTo"
+            :disabled="disabled"
+          >
+            <template #activator="{ on, attrs }">
+              <v-text-field
+                id="dateTo"
+                v-model="dateTo"
+                prepend-icon="mdi-calendar"
+                background-color="#37474fcc"
+                readonly
+                dark
+                dense
+                filled
+                outlined
+                v-bind="attrs"
+                :disabled="disabled"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
               v-model="dateTo"
-              prepend-icon="mdi-calendar"
-              background-color="#37474fcc"
-              readonly
+              scrollable
               dark
-              dense
-              filled
-              outlined
-              v-bind="attrs"
+              color="#A03636"
               :disabled="disabled"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="dateTo"
-            scrollable
-            dark
-            color="#A03636"
-            :disabled="disabled"
-            :min="dateStartedRecording"
-            :max="dateNow"
+              :min="dateStartedRecording"
+              :max="dateNow"
+            >
+              <v-spacer></v-spacer>
+              <v-btn class="btn" text @click="setDateToToNow()"> Today </v-btn>
+            </v-date-picker>
+          </v-dialog>
+          <label
+            class="text-center text-sm"
+            for="dateFrom"
+            :class="{ 'text-gray-600': disabled }"
+            >Date to</label
           >
-            <v-spacer></v-spacer>
-            <v-btn class="btn" text @click="setDateToToNow()"> Today </v-btn>
-          </v-date-picker>
-        </v-dialog>
-        <label
-          class="text-center text-sm"
-          for="dateFrom"
-          :class="{ 'text-gray-600': disabled }"
-          >Date to</label
-        >
+        </div>
+        <div class="inline-block">
+          <select
+            id="quickFilter"
+            v-model="quickFilter"
+            class="select-standard"
+            :disabled="disabled"
+          >
+            <option v-for="i in quickFilterItems" :key="i.val" :value="i.val">
+              {{ i.text }}
+            </option>
+          </select>
+          <label
+            class="text-center text-sm"
+            for="quickFilter"
+            :class="{ 'text-gray-600': disabled }"
+            >Quick Filter
+            <button class="label" :disabled="disabled">New!</button>
+          </label>
+        </div>
       </div>
-      <div class="inline-block">
-        <select
-          id="quickFilter"
-          v-model="quickFilter"
-          class="select-standard"
-          :disabled="disabled"
-        >
-          <option v-for="i in quickFilterItems" :key="i.val" :value="i.val">
-            {{ i.text }}
-          </option>
-        </select>
-        <label
-          class="text-center text-sm"
-          for="quickFilter"
-          :class="{ 'text-gray-600': disabled }"
-          >Quick Filter
-          <button class="label" :disabled="disabled">New!</button>
-        </label>
-      </div>
+    </div>
+    <div v-if="invalidDates" class="flex justify-center items-center">
+      <p class="text-red-500 font-bold">
+        Dates are invalid! "From" date must start before the "to" date and they
+        cannot be the same.
+      </p>
     </div>
   </div>
 </template>
@@ -146,6 +154,7 @@ export default Vue.extend({
       modalFrom: false,
       modalTo: false,
       waitingForData: false,
+      invalidDates: false,
       quickFilter: -1,
       quickFilterItems: [
         { text: 'All Time', val: -1 },
@@ -164,12 +173,10 @@ export default Vue.extend({
       this.waitingForData = false
     },
     dateFrom() {
-      console.log('new dateFromString', this.dateFrom)
       this.modalFrom = false
       this.updateIfBoth()
     },
     dateTo() {
-      console.log('new dateToString', this.dateTo)
       this.modalTo = false
       this.updateIfBoth()
     },
@@ -183,11 +190,18 @@ export default Vue.extend({
   },
   methods: {
     updateIfBoth() {
-      if (
-        !this.waitingForData &&
-        this.dateFrom.length > 0 &&
-        this.dateTo.length > 0
-      ) {
+      if (this.waitingForData) {
+        return
+      }
+
+      if (this.dateFrom > this.dateTo || this.dateFrom === this.dateTo) {
+        this.invalidDates = true
+        return
+      } else {
+        this.invalidDates = false
+      }
+
+      if (this.dateFrom.length > 0 && this.dateTo.length > 0) {
         console.log('ready to emit')
         this.emitDateUpdate()
       } else {
@@ -211,12 +225,10 @@ export default Vue.extend({
         from: dayjs(this.dateFrom).utc().unix(),
         to: dayjs(this.dateTo).utc().unix(),
       }
-      console.log('FilterDate: Emitting dates-changed', args)
       this.waitingForData = true
       this.$emit('dates-changed', args)
     },
     setDateFromToStart(): void {
-      console.log('setting to beginning of recording')
       this.dateFrom = dateStartedRecording
       this.modalFrom = false // Force close it in all cases
     },
