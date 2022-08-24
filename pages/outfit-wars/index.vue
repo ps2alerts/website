6029b7d572e0a9ce30acd2c51f60b9bd998ea376
Matt Cavanagh <maelstrome26@gmail.com>
2022-08-24 01:10:22 +0100

    Random 1am spitballing

--- pages/outfit-wars/index.vue
+++ pages/outfit-wars/index.vue
@@ -11,10 +11,69 @@
         src="https://giphy.com/embed/xTiIzJSKB4l7xTouE8"
         width="480"
         height="208"
-        frameBorder="0"
         class="giphy-embed m-auto"
         allowFullScreen
       ></iframe>
+    </div>
+    <div class="col-span-6">
+      <p>Obviously rough</p>
+      <v-card max-width="600" class="mx-auto" dark>
+        <v-toolbar color="light-blue" dark>
+          <v-toolbar-title>Miller</v-toolbar-title>
+        </v-toolbar>
+
+        <v-list subheader two-line>
+          <v-subheader inset>Qualified</v-subheader>
+
+          <v-list-item v-for="outfit in qualified" :key="outfit.title">
+            <v-list-item-avatar>
+              <v-icon class="grey lighten-1" dark> mdi-folder </v-icon>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title v-text="outfit.title"></v-list-item-title>
+
+              <v-list-item-subtitle
+                v-text="outfit.subtitle"
+              ></v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon color="grey lighten-1">mdi-information</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-subheader inset>Disqualified</v-subheader>
+
+          <v-list-item
+            v-for="outfit in disqualified"
+            :key="outfit.title"
+            disabled
+          >
+            <v-list-item-avatar>
+              <v-icon class="grey darken-1" dark> mdi-folder </v-icon>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title v-text="outfit.title"></v-list-item-title>
+
+              <v-list-item-subtitle
+                v-text="outfit.subtitle"
+              ></v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn icon>
+                <v-icon color="grey lighten-1">mdi-information</v-icon>
+              </v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
+      </v-card>
     </div>
   </section>
 </template>
@@ -34,6 +93,45 @@ export default Vue.extend({
       pageDesc: 'View all Rankings / Brackets for the Outfit Wars tournament!',
       // loading: false,
       // loaded: false,
+      qualified: [
+        {
+          icon: 'mdi-clipboard-text',
+          subtitle: '1st Place',
+          title: '[DIG] Dignity Of War',
+        },
+        {
+          icon: 'mdi-gesture-tap-button',
+          subtitle: '2nd Place',
+          title: '[VCBC] Vanu Creme Bun Community',
+        },
+        {
+          icon: 'mdi-gesture-tap-button',
+          subtitle: '3rd Place',
+          title: '[EDIM] Emerald Immerson',
+        },
+      ],
+      disqualified: [
+        {
+          icon: 'mdi-gesture-tap-button',
+          subtitle: '4th Place',
+          title: '[FOO] Foobar',
+        },
+        {
+          icon: 'mdi-gesture-tap-button',
+          subtitle: '5th Place',
+          title: '[EDIM] Emerald Immerson',
+        },
+        {
+          icon: 'mdi-gesture-tap-button',
+          subtitle: '6th Place',
+          title: '[EDIM] Emerald Immerson',
+        },
+        {
+          icon: 'mdi-gesture-tap-button',
+          subtitle: '7th Place',
+          title: '[EDIM] Emerald Immerson',
+        },
+      ],
     }
   },
   head(): object {
