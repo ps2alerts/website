e93d5df6aff94c070dabacda335bcb6b09b1e134
Ryan Sims <ryanjsims@email.arizona.edu>
2022-08-28 13:50:12 -0700

    Add 3xl @ 1920px and add true faction IDs with corner logos

--- components/outfitwars/TeamInfo.vue
+++ components/outfitwars/TeamInfo.vue
@@ -11,20 +11,22 @@
       v-else
       class="flex items-center flex-1"
     >
-      <div class="self-center place-self-center">
-        <object
-          :data="alert.outfitwars.teams.red.id | outfitImage"
-          type="image/png"
-          width="125px"
-        >
-          <img
-            :src="alert.outfitwars.teams.red.faction | factionImage"
-            :alt="alert.outfitwars.teams.red.faction | factionShortName"
-          />
-        </object>
+      <div class="aspect-square relative self-center place-self-center place-items-center w-12 lg:w-24 3xl:w-32">
+        <img
+          class="absolute inset-0 m-auto"
+          :src="alert.outfitwars.teams.red.id | outfitImage"
+          :alt="alert.outfitwars.teams.red.faction | factionShortName"
+          @error="imageUrlAltRed"
+        />
+        <img
+          ref="redFaction"
+          class="absolute right-0 bottom-0 w-1/4"
+          :src="alert.outfitwars.teams.red.faction | factionImage"
+          :alt="alert.outfitwars.teams.red.faction | factionShortName"
+        />
       </div>
       <div class="grid grid-cols-7 gap-2 justify-center grow">
-        <div class="col-start-1 col-span-3 align-bottom text-right text-xl mb-2 lg:mb-0">
+        <div class="col-start-1 col-span-3 align-bottom text-right 3xl:text-xl mb-2 lg:mb-0">
           <span v-if="alert.outfitwars.teams.red.tag">
             [{{ alert.outfitwars.teams.red.tag }}]
           </span>
@@ -33,24 +35,26 @@
         <div class="col-start-4 col-span-1 align-bottom text-center text-base mb-2 lg:mb-0">
             vs.
         </div>
-        <div class="col-end-8 col-span-3 align-bottom text-left text-xl mb-2 lg:mb-0">
+        <div class="col-end-8 col-span-3 align-bottom text-left 3xl:text-xl mb-2 lg:mb-0">
           <span v-if="alert.outfitwars.teams.blue.tag">
             [{{ alert.outfitwars.teams.blue.tag }}]
           </span>
           {{ alert.outfitwars.teams.blue.name.trim() }}
         </div>
       </div>
-      <div class="self-center place-self-center">
-        <object
-          :data="alert.outfitwars.teams.blue.id | outfitImage"
-          type="image/png"
-          width="125px"
-        >
-          <img
-            :src="alert.outfitwars.teams.blue.faction | factionImage"
-            :alt="alert.outfitwars.teams.blue.faction | factionShortName"
-          />
-        </object>
+      <div class="aspect-square relative self-center place-self-center place-items-center w-12 lg:w-24 3xl:w-32">
+        <img
+          class="absolute inset-0 m-auto"
+          :src="alert.outfitwars.teams.blue.id | outfitImage"
+          :alt="alert.outfitwars.teams.blue.faction | factionShortName"
+          @error="imageUrlAltBlue"
+        />
+        <img
+          ref="blueFaction"
+          class="absolute left-0 bottom-0 w-1/4"
+          :src="alert.outfitwars.teams.blue.faction | factionImage"
+          :alt="alert.outfitwars.teams.blue.faction | factionShortName"
+        />
       </div>
     </div>
   </div>
@@ -58,6 +62,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import factionImage from '~/filters/FactionImage'
 import { InstanceOutfitWarsResponseInterface } from '~/interfaces/InstanceOutfitWarsResponseInterface'
 
 export default Vue.extend({
@@ -70,6 +75,20 @@ export default Vue.extend({
       type: Object as () => InstanceOutfitWarsResponseInterface,
       default: {},
       required: true,
+    }
+  },
+  methods: {
+    imageUrlAltBlue(event: Event) {
+      if(event.target){
+        (event.target as HTMLImageElement).src = factionImage(this.alert.outfitwars?.teams?.blue?.faction);
+        (this.$refs["blueFaction"] as HTMLImageElement).classList.add("hidden");
+      }
+    },
+    imageUrlAltRed(event: Event) {
+      if(event.target){
+        (event.target as HTMLImageElement).src = factionImage(this.alert.outfitwars?.teams?.red?.faction)
+        (this.$refs["redFaction"] as HTMLImageElement).classList.add("hidden");
+      }
     }
   }
 })
