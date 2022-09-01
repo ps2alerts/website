3a5ab66bd4b425ec247590d84166cc7f75fee6d7
Matt Cavanagh <maelstrome26@gmail.com>
2022-09-01 12:43:01 +0100

    eslint fixes

--- components/outfitwars/RoundBracketEntry.vue
+++ components/outfitwars/RoundBracketEntry.vue
@@ -2,13 +2,14 @@
   <NuxtLink
     :to="{
       name: 'outfit-wars-outfitwars',
-      params: { 'outfitwars': match ? match.instanceId : ''},
+      params: { outfitwars: match ? match.instanceId : '' },
     }"
     :disabled="!match"
     :event="match && match.state !== 0 ? 'click' : ''"
-    
   >
-    <div class="flex gap-2 p-2 mb-2 bg-tint rounded relative hover bg-[#1e1e1e]">
+    <div
+      class="flex gap-2 p-2 mb-2 bg-tint rounded relative hover bg-[#1e1e1e]"
+    >
       <div class="self-center">
         <object
           :data="rankings[0].id | outfitImage"
@@ -28,9 +29,7 @@
           </div>
         </div>
         <div class="col-start-4 col-span-1 text-base mb-2 lg:mb-0">
-          <div class="text-center mb-1">
-            vs.
-          </div>
+          <div class="text-center mb-1">vs.</div>
         </div>
         <div class="col-end-8 col-span-3 text-base mb-2 lg:mb-0">
           <div class="text-left mb-1">
@@ -38,10 +37,16 @@
           </div>
         </div>
         <!-- Using borders here for alignment - the FactionSegment bar is 1px taller than the Awaiting match text otherwise -->
-        <div class="text-gray-500 col-start-3 col-span-3 border-b border-[#1e1e1e] rounded" v-if="!(match && match.result)">
-          <span>Match starts {{getUserTime(rankings[0].matchStartTime)}}</span>
+        <div
+          v-if="!(match && match.result)"
+          class="text-gray-500 col-start-3 col-span-3 border-b border-[#1e1e1e] rounded"
+        >
+          <span
+            >Match starts {{ getUserTime(rankings[0].matchStartTime) }}</span
+          >
         </div>
-        <FactionSegmentBar v-else-if="(match && match.result)"
+        <FactionSegmentBar
+          v-else-if="match && match.result"
           class="col-start-2 col-span-5"
           :vs="0"
           :nc="match.result.blue"
@@ -71,6 +76,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import moment from 'moment-timezone'
+import { PropValidator } from 'vue/types/options'
 import { DATE_TIME_FORMAT_SHORT } from '@/constants/Time'
 import { Team } from '@/ps2alerts-constants/outfitwars/team'
 import { FactionBgClass } from '@/constants/FactionBgClass'
@@ -80,13 +86,12 @@ import { InstanceOutfitWarsResponseInterface } from '~/interfaces/InstanceOutfit
 import { ParsedOutfitDataInterface } from '~/interfaces/ParsedOutfitDataInterface'
 import ApiRequest from '~/api-request'
 import { ps2AlertsApiEndpoints } from '~/ps2alerts-constants/ps2AlertsApiEndpoints'
-import { PropValidator } from 'vue/types/options'
 import { Ps2AlertsEventState } from '~/ps2alerts-constants/ps2AlertsEventState'
 
 export default Vue.extend({
   name: 'RoundBracketEntry',
   components: {
-    FactionSegmentBar
+    FactionSegmentBar,
   },
   props: {
     rankings: {
@@ -98,25 +103,25 @@ export default Vue.extend({
         for (const entry of value) {
           // each outfit must have an id, name, faction, world, and leader
           if (
-            entry.id === undefined 
-            || entry.name  === undefined 
-            || entry.faction  === undefined 
-            || entry.world  === undefined
-            || entry.matchStartTime === undefined
+            entry.id === undefined ||
+            entry.name === undefined ||
+            entry.faction === undefined ||
+            entry.world === undefined ||
+            entry.matchStartTime === undefined
           ) {
-            return false;
+            return false
           }
         }
-        return true;
+        return true
       },
-      required: true
-    } as PropValidator<ParsedOutfitDataInterface[]>
+      required: true,
+    } as PropValidator<ParsedOutfitDataInterface[]>,
   },
   data() {
     return {
       match: undefined as InstanceOutfitWarsResponseInterface | undefined,
       updateRate: 10000,
-      updateInterval: undefined as number | undefined
+      updateInterval: undefined as number | undefined,
     }
   },
   computed: {
@@ -143,52 +148,57 @@ export default Vue.extend({
     },
     victor(): Team | null {
       return this.match?.result ? this.match.result.victor : null
-    }
+    },
   },
   watch: {
     rankings: {
       handler() {
-        if(this.rankings[0].instanceId || this.rankings[1].instanceId) {
-          this.pull();
+        if (this.rankings[0].instanceId || this.rankings[1].instanceId) {
+          this.pull()
         }
       },
-      deep: true
-    }
+      deep: true,
+    },
   },
   created() {
-    this.pull();
-    this.updateInterval = window.setInterval(() => {
-      this.pull()
-    }, this.updateRate)
+    this.init()
   },
   methods: {
+    init() {
+      this.pull()
+      this.updateInterval = window.setInterval(() => {
+        this.pull()
+      }, this.updateRate)
+    },
     async pull() {
-      const instanceId = this.rankings[0].instanceId ?? this.rankings[1].instanceId;
-      if(instanceId) {
-        this.match = await new ApiRequest().get<InstanceOutfitWarsResponseInterface>(
-          ps2AlertsApiEndpoints.outfitwarsInstance.replace(
-            '{instanceId}',
-            instanceId
+      const instanceId =
+        this.rankings[0].instanceId ?? this.rankings[1].instanceId
+      if (instanceId) {
+        this.match =
+          await new ApiRequest().get<InstanceOutfitWarsResponseInterface>(
+            ps2AlertsApiEndpoints.outfitwarsInstance.replace(
+              '{instanceId}',
+              instanceId
+            )
           )
-        )
-        if(this.match.state === Ps2AlertsEventState.ENDED) {
-          window.clearInterval(this.updateInterval);
+        if (this.match.state === Ps2AlertsEventState.ENDED) {
+          window.clearInterval(this.updateInterval)
         }
       }
     },
     getUserTime(datetime: Date): string {
-      const locale = Intl.NumberFormat().resolvedOptions().locale;
+      const locale = Intl.NumberFormat().resolvedOptions().locale
       try {
         return new Intl.DateTimeFormat(locale, {
-          dateStyle: "short",
-          timeStyle: "short",
-        }).format(datetime);
+          dateStyle: 'short',
+          timeStyle: 'short',
+        }).format(datetime)
       } catch (err) {
-        console.error(err);
-        console.error(JSON.stringify(datetime));
-        return datetime.toLocaleString(locale);
+        console.error(err)
+        console.error(JSON.stringify(datetime))
+        return datetime.toLocaleString(locale)
       }
     },
-  }
+  },
 })
 </script>
