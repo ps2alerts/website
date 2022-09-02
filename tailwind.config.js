f07fe7728835852a239e8ee75e945324ddf755ef
Matt Cavanagh <maelstrome26@gmail.com>
2022-09-02 03:04:42 +0100

    Add grayscale class to safelist so it doesn't get culled

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,22 @@ module.exports = {
     'constants/**/*.ts',
     'filters/**/*.ts',
   ],
-  safelist: ['.nuxt-link-exact-active'],
+  safelist: ['.nuxt-link-exact-active', '.grayscale'],
   prefix: '',
   important: false,
   separator: ':',
   theme: {
     screens: {
-      'sm': '640px',
+      sm: '640px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: '1280px',
       // => @media (min-width: 1280px) { ... }
 
       '2xl': '1536px',
