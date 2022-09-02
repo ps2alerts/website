c5ecc661ab549cf82863e70ff97870e40c75d28d
Matt Cavanagh <maelstrome26@gmail.com>
2022-09-02 01:31:52 +0100

    prettier

--- interfaces/ParsedOutfitDataInterface.ts
+++ interfaces/ParsedOutfitDataInterface.ts
@@ -1,28 +1,28 @@
-import { Faction } from "~/ps2alerts-constants/faction"
-import { Phase } from "~/ps2alerts-constants/outfitwars/phase"
-import { World } from "~/ps2alerts-constants/world"
+import { Faction } from '~/ps2alerts-constants/faction'
+import { Phase } from '~/ps2alerts-constants/outfitwars/phase'
+import { World } from '~/ps2alerts-constants/world'
 
 export interface RankingInterface {
-    totalScore: number
-    played: number
-    wins: number
-    losses: number
-    tiebreaker: number
-    factionRank: number
-    globalRank: number
+  totalScore: number
+  played: number
+  wins: number
+  losses: number
+  tiebreaker: number
+  factionRank: number
+  globalRank: number
 }
-  
+
 export interface ParsedOutfitDataInterface {
-    id: string
-    matchStartTime: Date
-    name: string
-    tag: string | null
-    faction: Faction
-    world: World
-    round: number
-    phase: Phase
-    rankings: RankingInterface
-    outfitImageUrl: string
-    metricsString: string
-    instanceId: string | null
+  id: string
+  matchStartTime: Date
+  name: string
+  tag: string | null
+  faction: Faction
+  world: World
+  round: number
+  phase: Phase
+  rankings: RankingInterface
+  outfitImageUrl: string
+  metricsString: string
+  instanceId: string | null
 }
