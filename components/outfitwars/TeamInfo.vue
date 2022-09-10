95e1928aabc7bc0382f475685023404864ba578e
Matt Cavanagh <maelstrome26@gmail.com>
2022-09-10 22:48:38 +0100

    Optimized colourings and representation of winners for Team Info

--- components/outfitwars/TeamInfo.vue
+++ components/outfitwars/TeamInfo.vue
@@ -15,16 +15,6 @@
       Waiting for the first combat event...
     </div>
     <div v-else class="flex relative justify-between items-center flex-1">
-      <!--      <div-->
-      <!--        class="absolute -top-4 -left-4 w-4 md:w-6 xl:w-8 2xl:w-10 rounded"-->
-      <!--        :class="factionBackground(outfitwar.outfitwars.teams.red.faction)"-->
-      <!--      >-->
-      <!--        <img-->
-      <!--          class="w-4/5 m-auto"-->
-      <!--          :src="outfitwar.outfitwars.teams.red.faction | factionImage"-->
-      <!--          :alt="outfitwar.outfitwars.teams.red.faction | factionShortName"-->
-      <!--        />-->
-      <!--      </div>-->
       <TeamLogo
         class="self-center place-self-center w-12 lg:w-24 2xl:w-32"
         :outfit-id="outfitwar.outfitwars.teams.red.id"
@@ -33,7 +23,7 @@
       />
       <div class="relative place-self-stretch grow">
         <div
-          class="absolute top-0 left-2 text-xs sm:text-[1.5vw] xl:text-xl 2xl:text-2xl mb-2 lg:mb-0"
+          class="absolute top-0 left-2 text-xs sm:text-[1.5vw] xl:text-xl 2xl:text-2xl my-2 lg:mb-0"
         >
           <div class="leading-tight">
             <span v-if="outfitwar.outfitwars.teams.red.tag">
@@ -49,20 +39,17 @@
           </div>
           <div
             v-if="outfitwar.result && outfitwar.result.victor === 3"
-            class="text-xs label green"
+            class="text-xs label bg-amber-500"
           >
             Winner!
           </div>
-        </div>
-        <div class="absolute flex inset-0 place-content-center">
-          <div class="self-center xl:text-xl 2xl:text-2xl 3xl:text-3xl">vs</div>
         </div>
         <div
           class="absolute bottom-0 right-2 text-right text-xs md:text-[1.5vw] xl:text-xl 2xl:text-2xl mb-2 lg:mb-0"
         >
           <div
             v-if="outfitwar.result && outfitwar.result.victor === 2"
-            class="text-xs label green"
+            class="text-xs label bg-amber-500"
           >
             Winner!
           </div>
@@ -87,16 +74,6 @@
         :badge-left="true"
         :loser="outfitwar.result && outfitwar.result.victor === 3"
       />
-      <!--      <div-->
-      <!--        class="absolute -right-4 -bottom-4 w-4 md:w-6 xl:w-8 2xl:w-10 rounded"-->
-      <!--        :class="factionBackground(outfitwar.outfitwars.teams.blue.faction)"-->
-      <!--      >-->
-      <!--        <img-->
-      <!--          class="w-4/5 m-auto"-->
-      <!--          :src="outfitwar.outfitwars.teams.blue.faction | factionImage"-->
-      <!--          :alt="outfitwar.outfitwars.teams.blue.faction | factionShortName"-->
-      <!--        />-->
-      <!--      </div>-->
     </div>
   </div>
 </template>
@@ -152,28 +129,28 @@ export default Vue.extend({
 .teams-background {
   background: linear-gradient(
     150deg,
-    rgba(155, 44, 44, 0.5) 0 51.5%,
-    black,
-    rgba(43, 108, 176, 0.5) 52% 100%
+    rgba(155, 44, 44, 0.5) 0 51.75%,
+    white,
+    rgba(43, 108, 176, 0.5) 52.2% 100%
   );
 
   &.winner-red {
     background: linear-gradient(
       150deg,
-      rgba(155, 44, 44, 0.9) 0 51.5%,
-      black,
+      rgba(155, 44, 44, 0.9) 0 51.75%,
+      red,
       rgba(155, 44, 44, 0.2),
-      rgba(43, 108, 176, 0.5) 52.75% 100%
+      rgba(43, 108, 176, 0.5) 52.2% 100%
     );
   }
 
   &.winner-blue {
     background: linear-gradient(
       150deg,
-      rgba(155, 44, 44, 0.5) 0 50.75%,
+      rgba(155, 44, 44, 0.5) 0 51.75%,
       rgba(43, 108, 176, 0.2),
-      black,
-      rgba(43, 108, 176, 0.9) 52% 100%
+      #53bcff,
+      rgba(43, 108, 176, 0.9) 52.2% 100%
     );
   }
 }
